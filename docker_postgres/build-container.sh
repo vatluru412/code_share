419c6f507925558347d554c831eeb101a1d5392b
TG <tgronka@cisco.com>
2020-06-23 11:19:24 -0500

    better image name

--- docker_postgres/build-container.sh
+++ docker_postgres/build-container.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo docker rm mypostgres
-sudo docker build -t mypostgrescontainer -f ./postgres.dockerfile ./
-sudo docker run --name mypostgres mypostgrescontainer
+sudo docker build -t mypostgresimage -f ./postgres.dockerfile ./
+sudo docker run --name mypostgres mypostgresimage
 #sudo docker run -p 5432:5432 --name mypostgres mypostgres
--- docker_postgres/start-postgres.sh
+++ docker_postgres/start-postgres.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker start myswapi
+sudo docker start mypostgres

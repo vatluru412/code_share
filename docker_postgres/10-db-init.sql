54fae1d5ebd2c27e455fee9452252932ccc9c332
TG <tgronka@cisco.com>
2020-06-23 11:05:49 -0500

    psql extension is skipped :(

--- docker_postgres/10-db-init.sql
+++ docker_postgres/10-db-init.sql
@@ -8,16 +8,16 @@ ALTER USER postgres WITH PASSWORD 'newpassword';
 
 CREATE TABLE characters (
 	char_uuid UUID NOT NULL,
-	charname TEXT,
-	PRIMARY KEY (charid)
+	character_name TEXT,
+	PRIMARY KEY (char_uuid)
 );
 
 CREATE TABLE films (
 	film_uuid UUID NOT NULL,
 	char_uuid UUID NOT NULL,
 	title TEXT,
-	PRIMARY KEY (id)
+	PRIMARY KEY (film_uuid)
 );
 
-INSERT INTO characters (char_uuid, title) VALUES ( '00000000-0000-0000-0000-000000000001', 'The Phantom Menace');
-INSERT INTO characters (char_uuid, title) VALUES ( '00000000-0000-0000-0000-000000000002', 'The Clone Wars');
+INSERT INTO characters (char_uuid, character_name) VALUES ( '00000000-0000-0000-0000-000000000001', 'Luke');
+INSERT INTO characters (char_uuid, character_name) VALUES ( '00000000-0000-0000-0000-000000000002', 'Yoda');
--- docker_postgres/postgres.dockerfile
+++ docker_postgres/postgres.dockerfile
@@ -2,6 +2,6 @@ FROM postgres:10.5
 ENV POSTGRES_USER postgres
 ENV POSTGRES_PASSWORD postgres
 ENV POSTGRES_DB postgres
-ADD 10-db-init.pgsql /docker-entrypoint-initdb.d/
+ADD 10-db-init.sql /docker-entrypoint-initdb.d/
 #ADD 11-db-characters.pgsql /docker-entrypoint-initdb.d/
 EXPOSE 5432
